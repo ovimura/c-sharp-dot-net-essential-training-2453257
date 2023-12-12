a9c225509e75c3ab2adda0005f072704c955613c
Ovidiu Mura <ovidiu.mura@yahoo.com>
2023-12-12 09:32:46 -0800

    adding solution for problem - NumberDates

---
+++ Start/NumbersDates/Challenge/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+string exit = "exit";
+string input = "";
+DateTime today = DateTime.Today;
+
+do
+{
+    Console.WriteLine("Which date? (or 'exit')");
+    input = Console.ReadLine();
+    DateTime dt;
+    if(DateTime.TryParse(input, out dt)) {
+        if(dt == today) {
+            Console.WriteLine($"That is today, {dt.ToString("dd MMM yyyy")}");
+        } else if (dt > today) {
+            Console.WriteLine($"That is in the future, {(dt-today).Days} days.");
+        } else if(dt < today) {
+            Console.WriteLine($"That is in the past, {(today-dt).Days} days.");
+        }
+    } else if (input != exit) {
+        Console.WriteLine("That doesn't seem to be a valid date");
+    }
+} while (input != exit);
